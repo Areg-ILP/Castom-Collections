a675a98e63a0e5a4356845f5b9600be3fd957de6
Areg-ILP <61777021+Areg-ILP@users.noreply.github.com>
2021-01-24 15:14:46 +0400

    Add files via upload

---
+++ _Queue.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Castom_Collection
+{
+    public class _Queue<T>
+    {
+        private T[] _array;
+        private int _size;
+        private int _head;
+        private int _tail;
+        private int _version;
+
+        private const int _MinimumGrow = 4;
+        private const int _ShrinkThreshold = 32;
+        private const int _GrowFactor = 200;
+        private const int _defaultCapacity = 4;
+
+        public _Queue(int capacity = _defaultCapacity)
+        {
+            if(capacity <= 0)            
+                throw new ArgumentOutOfRangeException();
+
+            _array = new T[capacity];
+            _size = 0;
+            _tail = 0;
+            _version = 0;
+        }
+
+        public int Count => _size;
+
+        public T Peek()
+        {
+            if (_size == 0)
+                throw new InvalidOperationException();
+            
+            return _array[_head];
+        }
+        public T Dequeue()
+        {
+            if (_size == 0)
+                throw new InvalidOperationException();
+
+            T removed = _array[_head];
+            _array[_head] = default(T);
+            _head = (_head + 1) % _array.Length;
+            _size--;
+            _version++;
+            return removed;
+        }
+
+        public void Enqueue(T item)
+        {
+            if (_size == _array.Length)
+            {
+                int newcapacity = (int)((long)_array.Length * (long)_GrowFactor / 100);
+                if (newcapacity < _array.Length + _MinimumGrow)
+                {
+                    newcapacity = _array.Length + _MinimumGrow;
+                }
+                SetCapacity(newcapacity);
+            }
+
+            _array[_tail] = item;
+            _tail = (_tail + 1) % _array.Length;
+            _size++;
+            _version++;
+        }
+
+        private void SetCapacity(int capacity)
+        {
+            T[] newarray = new T[capacity];
+            if (_size > 0)
+            {
+                if (_head < _tail)
+                {
+                    Array.Copy(_array, _head, newarray, 0, _size);
+                }
+                else
+                {
+                    Array.Copy(_array, _head, newarray, 0, _array.Length - _head);
+                    Array.Copy(_array, 0, newarray, _array.Length - _head, _tail);
+                }
+            }
+
+            _array = newarray;
+            _head = 0;
+            _tail = (_size == capacity) ? 0 : _size;
+            _version++;
+        }
+
+        public void Clear()
+        {
+            if (_head < _tail)
+                Array.Clear(_array, _head, _size);
+            else
+            {
+                Array.Clear(_array, _head, _array.Length - _head);
+                Array.Clear(_array, 0, _tail);
+            }
+
+            _head = 0;
+            _tail = 0;
+            _size = 0;
+            _version++;
+        }
+    }
+}
