f00bed50e891e1f7b2b1b3d027825839474434b3
Areg-ILP <61777021+Areg-ILP@users.noreply.github.com>
2021-01-25 22:35:58 +0400

    Add files via upload

---
+++ HashHelper.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Castom_Collection
+{
+    public static class HashHelper
+    {
+        // ? bac toxac parzer
+        private static readonly int[] _primes = {
+            3, 7, 11, 17, 23, 29, 37, 47, 59, 71, 89, 107, 131, 163, 197, 239, 293, 353, 431, 521, 631, 761, 919,
+            1103, 1327, 1597, 1931, 2333, 2801, 3371, 4049, 4861, 5839, 7013, 8419, 10103, 12143, 14591,
+            17519, 21023, 25229, 30293, 36353, 43627, 52361, 62851, 75431, 90523, 108631, 130363, 156437,
+            187751, 225307, 270371, 324449, 389357, 467237, 560689, 672827, 807403, 968897, 1162687, 1395263,
+            1674319, 2009191, 2411033, 2893249, 3471899, 4166287, 4999559, 5999471, 7199369
+        };
+
+        private static int _current;
+
+        static HashHelper()
+        {
+            _current = 0;
+        }
+
+        public static int MinPrime => _primes[0];              
+        public static int CurrentPrime => _primes[_current++];
+
+        public static int GetPrime(int number)
+        {
+            if (IsPrime(number))
+                return number;
+            int nearPrime = GetNearPrime(number);
+            _current = GetIndex(nearPrime);
+            return _primes[_current++];            
+        }
+
+        private static bool IsPrime(int number)
+        {
+            if (number < 3)
+                return false;
+            for (int i = 2; i <= number / 2; i++)
+                if (number % i == 0)
+                    return false;
+            return true;
+        }
+
+        private static int GetNearPrime(int number)
+        {
+            if (IsPrime(number))
+                return number;
+            do
+            {
+                number++;
+            } while (!IsPrime(number));
+            return number;
+        }
+
+        private static int GetIndex(int prime)
+        {
+            for (int i = 0; i < _primes.Length; i++)
+                if (_primes[i] == prime)
+                    return i;
+            return -1;
+        }
+    }
+}
---
+++ _HashSet.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Castom_Collection
+{
+    // ? indexer
+    public class _HashSet<T>
+    {
+        public struct Slot
+        {
+            internal int hashCode;
+            internal int next;
+            internal T value;
+        }
+
+        private int[] _buckets;
+        private Slot[] _slots;
+        private int _count;
+        private int _freeList;
+        private int _lastIndex;
+        private int _version;
+
+        public int Count => _count;
+
+        public _HashSet() : this(0) {}
+
+        public _HashSet(int capacity)
+        {
+            if (capacity < 0)
+                throw new ArgumentOutOfRangeException();
+            Intilizie(capacity);
+        }
+
+        public bool Add(T value)
+        {
+            return AddIfNotPresent(value);
+        }
+
+        private bool AddIfNotPresent(T value)
+        {
+            if (_buckets == null)
+                Intilizie(0);
+
+            int hashCode = GetHashCode(value);
+            int bucket = hashCode % _buckets.Length;
+
+            for (int i = _buckets[hashCode % _buckets.Length] - 1; i >= 0; i = _slots[i].next)
+            {
+                if (_slots[i].hashCode == hashCode && _slots[i].value.Equals(value))
+                {
+                    return false;
+                }
+            }
+
+            int index;
+            if(_freeList >= 0)
+            {
+                index = _freeList;
+                _freeList = _slots[index].next;
+            }
+            else
+            {
+                if(_lastIndex == _slots.Length)
+                {
+                    IncreaseCapacity();
+                    bucket = hashCode % _slots.Length;
+                }
+                index = _lastIndex;
+                _lastIndex++;   
+            }
+
+            _slots[index].hashCode = hashCode;
+            _slots[index].value = value;
+            _slots[index].next = _buckets[bucket] - 1;
+            _buckets[bucket] = index + 1;
+            _count++;
+            _version++;
+
+            return true;
+        }
+
+        public bool Contains(T item)
+        {
+            if (_buckets != null)
+            {
+                int hashCode = GetHashCode(item);                
+                for (int i = _buckets[hashCode % _buckets.Length] - 1; i >= 0; i = _slots[i].next)
+                {
+                    if (_slots[i].hashCode == hashCode && _slots[i].value.Equals(item))
+                    {
+                        return true;
+                    }
+                }
+            }            
+            return false;
+        }
+
+        public void Clear()
+        {
+            if (_lastIndex > 0)
+            {
+                Array.Clear(_slots, 0, _lastIndex);
+                Array.Clear(_buckets, 0, _buckets.Length);
+                _lastIndex = 0;
+                _count = 0;
+                _freeList = -1;
+            }
+            _version++;
+        }
+
+        public bool TryGetValue(T equalValue, out T actualValue)
+        {
+            if (_buckets != null)
+            {
+                int i = IndexOf(equalValue);
+                if (i >= 0)
+                {
+                    actualValue = _slots[i].value;
+                    return true;
+                }
+            }
+            actualValue = default(T);
+            return false;
+        }
+
+        public int IndexOf(T item)
+        {           
+            int hashCode = GetHashCode(item);
+            for (int i = _buckets[hashCode % _buckets.Length] - 1; i >= 0; i = _slots[i].next)
+            {
+                if ((_slots[i].hashCode) == hashCode && _slots[i].value.Equals(item))
+                {
+                    return i;
+                }
+            }            
+            return -1;
+        }
+
+        private int GetHashCode(T item)
+        {
+            if (item == null)            
+                return 0;            
+            return item.GetHashCode() & 0x7FFFFFFF;
+        }
+
+        private void Intilizie(int capacity)
+        {
+            int size = HashHelper.GetPrime(capacity);
+            _buckets = new int[size];            
+            _slots = new Slot[size];
+            _freeList = -1;
+        }
+
+        private void IncreaseCapacity()
+        {
+            int newSize = HashHelper.CurrentPrime;
+            if (newSize <= _count)
+                throw new ArgumentOutOfRangeException();
+            SetCapacity(newSize);
+        }
+
+        private void SetCapacity(int size)
+        {
+            Slot[] newSlots = new Slot[size];
+            if (_slots != null)
+            {
+                Array.Copy(_slots, 0, newSlots, 0, _lastIndex);
+            }
+
+            int[] newBuckets = new int[size];
+            for (int i = 0; i < _lastIndex; i++)
+            {
+                int bucket = newSlots[i].hashCode % size;
+                newSlots[i].next = newBuckets[bucket] - 1;
+                newBuckets[bucket] = i + 1;
+            }
+
+            _slots = newSlots;
+            _buckets = newBuckets;
+        }
+    }
+}
